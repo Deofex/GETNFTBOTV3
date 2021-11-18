06c6953b36878bf36ac316666b3d68327ec57532
deofex <roy@powerplatz.nl>
2021-11-18 16:11:43 +0100

    Add Docker support

---
+++ UpcomingEventsReport/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Start the scheduler (logging in /var/log/script.log)"
+
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
+# Setup a cron schedule
+echo "SHELL=/bin/bash
+BASH_ENV=/container.env
+0 16 * * * (cd /app;python /app/main.py) >> /var/log/script.log 2>&1
+# Cronjob to run the reporter" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
