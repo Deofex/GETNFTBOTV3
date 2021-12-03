b110ee5e679d86e7d7395e0e827e6e3738ae1168
deofex <roy@powerplatz.nl>
2021-12-03 08:20:50 +0100

    Add support for max skip limit

--- DailyReport/qgraph/graphdayreport.py
+++ DailyReport/qgraph/graphdayreport.py
@@ -12,14 +12,27 @@ def graphDayReport(date):
     epochdate = datetime(1970, 1, 1)
     datenumber = (date - epochdate).days
     skip = 0
+    minblocknumber = 0
     events = []
     # Gather data, max 100 entries per time, so keep looping until all data is
     # gathered.
     while True:
+        # If skip is greated than 5000, set new minimal blocknumber, remove the
+        # events which has this blocknumber to avoid incomplete results and
+        # reset the skip var.
+        if skip > 5000:
+            minblocknumber = events[-1]['blockNumber']
+            events = [d for d in events \
+                if d['blockNumber'] < minblocknumber]
+            skip = 0
         q = '''
         {
-            usageEvents(orderBy: id, where:{day:%s}, first:100, skip:%s)
+            usageEvents(
+                orderBy: blockNumber,
+                where:{day:%s, blockNumber_gte: %s}, first:100, skip:%s
+                )
             {
+                blockNumber
                 type
                 event{
                     eventName
@@ -27,7 +40,7 @@ def graphDayReport(date):
                 }
             }
         }
-        ''' % (datenumber, skip)
+        ''' % (datenumber, minblocknumber, skip)
         data = querygraph(q)
         events = events + data['data']['usageEvents']
 
